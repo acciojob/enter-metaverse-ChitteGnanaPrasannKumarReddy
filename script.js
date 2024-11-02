a129edd0f1f9a30f4489e664965ab342203fecd8
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-11-02 22:18:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const statusParagraph = document.getElementById('status');
+  const enterButton = document.getElementById('enterBtn');
+
+  enterButton.addEventListener('click', function() {
+    
+    statusParagraph.innerHTML = "<h1>Entered Metaverse</h1>";
+  });
+});
